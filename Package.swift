9674b15b857494e39de46b43c750a754d216744b
Shams Ahmed <shamsahmed@me.com>
2020-09-28 17:26:45 +0100

    exclude

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let version = "1.0.5"
 let name = "Hello"
 let product: Product = .library(name: name, targets: [name])
 let url = "https://github.com/shams-ahmed/Hello/releases/download/1.0.3/Hello.xcframework.zip"
-var target: Target = .binaryTarget(
+let target: Target = .binaryTarget(
     name: name,
     url: url,
     checksum: "7583892ab327399bef0ba3a7b626deea6ffe91cc939c04d9ac7f959c4a097d3d"
