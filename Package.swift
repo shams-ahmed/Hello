5c5b609f315d8010af65308da8fe46753b0f00bc
Shams Ahmed <shamsahmed@me.com>
2020-09-28 17:19:37 +0100

    exclude

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let version = "1.0.4"
+let version = "1.0.5"
 let name = "Hello"
 let product: Product = .library(name: name, targets: [name])
 let url = "https://github.com/shams-ahmed/Hello/releases/download/1.0.3/Hello.xcframework.zip"
@@ -10,7 +10,6 @@ var target: Target = .binaryTarget(
     url: url,
     checksum: "7583892ab327399bef0ba3a7b626deea6ffe91cc939c04d9ac7f959c4a097d3d"
 )
-target.exclude = ["libHello.a"]
 
 let package = Package(
     name: name,
