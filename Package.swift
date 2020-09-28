491ae46b9ec1179b65124ab10657cc1c7c712b29
Shams Ahmed <shamsahmed@me.com>
2020-09-28 11:22:18 +0100

    1.0.2

--- Package.swift
+++ Package.swift
@@ -1,19 +1,19 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let version = "1.0.0"
+let version = "1.0.2"
 let name = "Hello"
 let product: Product = .library(name: name, targets: [name])
-let url = "https://github.com/shams-ahmed/Hello/releases/download/1.0.0/Hello.xcframework.zip"
+let url = "https://github.com/shams-ahmed/Hello/releases/download/1.0.2/Hello.xcframework.zip"
 let target: Target = .binaryTarget(
     name: name,
     url: url,
-    checksum: "e21d72e1781fea97bbe21ff92d64471240acb0ffaffb9c04b318b2711433d847"
+    checksum: "7583892ab327399bef0ba3a7b626deea6ffe91cc939c04d9ac7f959c4a097d3d"
 )
 
 let package = Package(
     name: name,
-    platforms: [.iOS(.v10)],
+    platforms: [.iOS(.v14)],
     products: [product],
     targets: [target],
     swiftLanguageVersions: [.v5]
